661e57dbdb0848cb37d3551e9e37b2adbf384d5a
张峻睿 <findream@zhangjunruideMacBook-Pro.local>
2024-01-20 16:37:26 +0800

    feat: add swagger

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"gochat/docs"
 	"gochat/router"
 	"gochat/utils"
+
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -10,6 +14,8 @@ func main() {
 	utils.InitMysql()
 
 	r := router.Router()
+	docs.SwaggerInfo.BasePath = ""
+	r.GET("/swragger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
